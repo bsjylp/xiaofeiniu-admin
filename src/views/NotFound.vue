bd0f57715f2a18976732503b04ecd3205e9b6794
bsj <bsj@123.cn>
2019-01-30 18:04:31 +0800

    初始组件

---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="yb-notfound">
+        <h1>404! NOT FOUND</h1>
+    </div>
+</template>
+
+<style lang="scss">
+    .yb-notfound{
+        text-align: center;
+        padding-top:200px;
+    }
+</style>
